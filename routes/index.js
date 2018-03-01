549021ab6d0a1f37cac921921e96689675bf3ee5
Scott Bowler <scott.bowler@learningfuze.com>
2018-02-28 17:46:12 -0800

    Updated server files for react front end

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,13 @@
 const { sendData, getData, syncSheets } = require('../services/google_sheet');
 
 module.exports = app => {
-    // app.get('/', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
-    // });
-
     // app.post('/get-data', getData);
-
-    app.get('/', (req, res) => {
-	res.send('<h1>Site status: [OK]</h1>');
-    });
 
     app.post('/send-data', sendData);
 
     app.get('/sync-sheets', syncSheets);
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'));
+    });
 }
