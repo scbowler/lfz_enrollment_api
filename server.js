80e0054a89ee9b986cd60c70be73c80da3b2f2ea
Scott Bowler <scott.bowler@learningfuze.com>
2020-03-05 12:51:10 -0800

    Removed https setup, to be handled by nginx

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const https = require('https');
 const path = require('path');
 const cors = require('cors');
-const { sslOptions, dbConnect } = require('./config');
 const routes = require('./routes');
 const authRoutes = require('./routes/auth');
 const apiRoutes = require('./routes/api');
 const PORT = process.env.PORT || 9000;
-const ENV = process.env.ENV || 'production';
 
 const app = express();
 
@@ -20,17 +17,6 @@ authRoutes(app);
 apiRoutes(app);
 routes(app);
 
-switch(ENV){
-    case 'production':
-        https.createServer(sslOptions, app).listen(443, () => {
-            console.log('HTTPS server running on PORT:443');
-        });
-        break;
-    case 'livedev':
-        exports.app = app;
-        break;
-    default:
-        app.listen(PORT, () => {
-            console.log('Server running on PORT: ' + PORT);
-        });
-}
+app.listen(PORT, () => {
+    console.log('LFZ Enrollment API running on PORT: ' + PORT);
+});
